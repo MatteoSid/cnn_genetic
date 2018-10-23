3046812342685481beeb90516bca32f471eff468
MatteoSid <matteo0593@gmail.com>
2018-10-23 15:29:00 +0200

    Aggiunti gli scope per tensorboard a cnn_mnist_beta4.py

--- cnn_mnist_beta4.py
+++ cnn_mnist_beta4.py
@@ -25,8 +25,8 @@ import os
 
 os.system('clear')
 
-#MODE='TRAIN'
-MODE='TEST'
+MODE='TRAIN'
+# MODE='TEST'
 
 USER='Matteo'
 #USER='Giacomo'
@@ -65,69 +65,78 @@ iterator = dataset.make_initializable_iterator()
 
 
 def cnn_model_fn(x):
+    
     # INPUT LAYER
-    input_layer = tf.reshape(x, [-1, 28, 28, 1])
+    with tf.name_scope('input_layer') as scope:
+        input_layer = tf.reshape(x, [-1, 28, 28, 1])
 
     # CONVOLUTIONAL LAYER #1
-    conv1 = tf.layers.conv2d(
-        inputs=input_layer,
-        filters=32,
-        kernel_size=[5, 5],
-        padding="same",
-    )
-
-    # APPLICO LA FUNZIONE RELU
-    conv1_relu = tf.nn.relu(conv1)
+    with tf.name_scope('Conv1') as scope:
+        conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=32,
+            kernel_size=[5, 5],
+            padding="same",
+        )
+        # APPLICO LA FUNZIONE RELU
+        conv1_relu = tf.nn.relu(conv1)
 
     # POOLING LAYER #1
-    pool1 = tf.layers.max_pooling2d(
-        inputs=conv1_relu,
-        pool_size=[2, 2],
-        strides=2
-    )
+    with tf.name_scope('Pool1'):
+        pool1 = tf.layers.max_pooling2d(
+            inputs=conv1_relu,
+            pool_size=[2, 2],
+            strides=2
+        )
 
     # CONVOLUTIONAL LAYER #2
-    conv2 = tf.layers.conv2d(
-        inputs=pool1,
-        filters=64,
-        kernel_size=[5, 5],
-        padding="same",
-    )
+    with tf.name_scope('Conv2'):
+        conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=64,
+            kernel_size=[5, 5],
+            padding="same",
+        )
 
-    # APPLICO LA FUNZIONE RELU
-    conv2_relu = tf.nn.relu(conv2)
+        # APPLICO LA FUNZIONE RELU
+        conv2_relu = tf.nn.relu(conv2)
 
     # POOLING LAYER #2
-    pool2 = tf.layers.max_pooling2d(
-        inputs=conv2_relu,
-        pool_size=[2, 2],
-        strides=2
-    )
+    with tf.name_scope('Pool2'):
+        pool2 = tf.layers.max_pooling2d(
+            inputs=conv2_relu,
+            pool_size=[2, 2],
+            strides=2
+        )
 
     # RIDIMENSIONO POOL2 PER RIDURRE IL CARICO COMPUTAZIONALE
-    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    with tf.name_scope('Reshape'):
+        pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
 
     # DENSE LAYER
-    dense = tf.layers.dense(
-        inputs=pool2_flat,
-        units=1024,
-    )
+    with tf.name_scope('Dense_layer'):
+        dense = tf.layers.dense(
+            inputs=pool2_flat,
+            units=1024,
+        )
 
-    # APPLICO LA FUNZIONE RELU
-    dense_relu = tf.nn.relu(dense)
+        # APPLICO LA FUNZIONE RELU
+        dense_relu = tf.nn.relu(dense)
 
     # AGGIUNGO L'OPERAZIONE DI DROPOUT
-    dropout = tf.layers.dropout(
-        inputs=dense_relu, 
-        rate=0.4,
-        training = MODE == tf.estimator.ModeKeys.TRAIN
-    )
+    with tf.name_scope('Dropout'):
+        dropout = tf.layers.dropout(
+            inputs=dense_relu, 
+            rate=0.4,
+            training = MODE == tf.estimator.ModeKeys.TRAIN
+        )
 
     # LOGIT LAYER
-    logits = tf.layers.dense(
-        inputs=dropout,
-        units=10
-    )
+    with tf.name_scope('Logit_layer'):
+        logits = tf.layers.dense(
+            inputs=dropout,
+            units=10
+        )
 
     return logits
 
