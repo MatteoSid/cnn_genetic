92f8f9b36b8daa6246a024959e28e47e80442b59
MatteoSid <matteo0593@gmail.com>
2019-01-30 14:10:16 +0100

    Aggiornamento di dataset_creator
    * ora SELECTION e NEUTRAL vengono salvati nella cartella DATASET;
    * aggiunta l'opzione per eliminare le matrici in formato di testo
    * aggiunta l'opzione per creare l'archivio con il dataset

--- dataset_creator/dataset_creator.py
+++ dataset_creator/dataset_creator.py
@@ -20,7 +20,7 @@ os.system('rm -rf *.sim')
 os.system('clear')
 
 
-path = os.getcwd() + '/dataset_creator/'
+path = os.getcwd() + '/'
 print('Path corrente: ' + path)
 
 print('\nComando generico:\npython3 ms2raster.py -bp 10000 -s 2 -l selection -selstr 0.005 -p ' + path + ' -i 24\n')
@@ -37,11 +37,6 @@ n_train = int(input('Inserire quante matrici per il training set (-s): '))
 n_test = int(input('Inserire quante matrici per il test set (-s): '))
 selstr = input('Inserire un valore per selstr (-selstr): ')
 
-#n_min = 1000
-#n_max = 1050
-#passo = 10
-#n = 5
-
 create_tree(
     path=path, 
     mode=mod
@@ -50,78 +45,86 @@ create_tree(
 os.system('clear')
     
 if mod == 'S' or mod == 'B':
-    comand_train = 'cd ' + path + 'SELECTION/TRAIN ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_train) + ' -l selection -selstr ' + selstr + ' -p ' + path + 'SELECTION/TRAIN/ -i +24'
+    comand_train = 'cd ' + path + 'DATASET/SELECTION/TRAIN ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_train) + ' -l selection -selstr ' + selstr + ' -p ' + path + 'DATASET/SELECTION/TRAIN/ -i +24'
     print('[LOG:comand_train]: ' + comand_train)
     # os.system(comand_train)   
     
-    comand_test = 'cd ' + path + 'SELECTION/TEST ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_test) + ' -l selection -selstr ' + selstr + ' -p ' + path + 'SELECTION/TEST/ -i +24'
+    comand_test = 'cd ' + path + 'DATASET/SELECTION/TEST ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_test) + ' -l selection -selstr ' + selstr + ' -p ' + path + 'DATASET/SELECTION/TEST/ -i +24'
     print('\nLOG:comand_test]: ' + comand_test)
     # os.system(comand_test)   
 
 if mod == 'N' or mod == 'B':
-    comand_train = 'cd ' + path + 'NEUTRAL/TRAIN ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_train) + ' -l neutral -selstr ' + selstr + ' -p ' + path + 'NEUTRAL/TRAIN/ -i +24'
+    comand_train = 'cd ' + path + 'DATASET/NEUTRAL/TRAIN ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_train) + ' -l neutral -selstr ' + selstr + ' -p ' + path + 'DATASET/NEUTRAL/TRAIN/ -i +24'
     print('[LOG:comand_train]: ' + comand_train)
     # os.system(comand_train)   
     
-    comand_test = 'cd ' + path + 'NEUTRAL/TEST ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_test) + ' -l neutral -selstr ' + selstr + ' -p ' + path + 'NEUTRAL/TEST/ -i +24'
+    comand_test = 'cd ' + path + 'DATASET/NEUTRAL/TEST ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(n_test) + ' -l neutral -selstr ' + selstr + ' -p ' + path + 'DATASET/NEUTRAL/TEST/ -i +24'
     print('\nLOG:comand_test]: ' + comand_test)
     # os.system(comand_test)
 
 if input('\nVuoi trasformare le matrici in immagini? [Y]/[N]: ') == 'Y': 
 
     if mod == 'S' or mod == 'B':
-        os.mkdir(path + 'SELECTION/TRAIN_IMG')
-        os.mkdir(path + 'SELECTION/TEST_IMG')
+        os.mkdir(path + 'DATASET/SELECTION/TRAIN_IMG')
+        os.mkdir(path + 'DATASET/SELECTION/TEST_IMG')
 
         print('\nTrasformo il selection training set in immagini...')
         for i in tqdm(range(1, n_train+1)):
             # SELECTION - TRAIN
-            path_tmp_strain = path + 'SELECTION/TRAIN/' + str(i) + '.selection.sim'
+            path_tmp_strain = path + 'DATASET/SELECTION/TRAIN/' + str(i) + '.selection.sim'
             file_strain = Path(path_tmp_strain)
             if file_strain.is_file():
                 converter_fn(
-                            path = path + 'SELECTION/TRAIN/' + str(i) + '.selection.sim',
-                            file_name = path + 'SELECTION/TRAIN_IMG/' + str(i) + '.selection.png',
+                            path = path + 'DATASET/SELECTION/TRAIN/' + str(i) + '.selection.sim',
+                            file_name = path + 'DATASET/SELECTION/TRAIN_IMG/' + str(i) + '.selection.png',
                             )
 
         print('\nTrasformo il selection test set in immagini...')
         for i in tqdm(range(1, n_test+1)):
             # SELECTION - TEST
-            path_tmp_stest = path + 'SELECTION/TEST/' + str(i) + '.selection.sim'
+            path_tmp_stest = path + 'DATASET/SELECTION/TEST/' + str(i) + '.selection.sim'
             file_stest = Path(path_tmp_stest)
             if file_stest.is_file():
                 converter_fn(
-                            path = path + 'SELECTION/TEST/' + str(i) + '.selection.sim',
-                            file_name = path + 'SELECTION/TEST_IMG/' + str(i) + '.selection.png',
+                            path = path + 'DATASET/SELECTION/TEST/' + str(i) + '.selection.sim',
+                            file_name = path + 'DATASET/SELECTION/TEST_IMG/' + str(i) + '.selection.png',
                             )
     if mod == 'N' or mod == 'B':
-        os.mkdir(path + 'NEUTRAL/TRAIN_IMG')
-        os.mkdir(path + 'NEUTRAL/TEST_IMG')
+        os.mkdir(path + 'DATASET/NEUTRAL/TRAIN_IMG')
+        os.mkdir(path + 'DATASET/NEUTRAL/TEST_IMG')
 
         print('\nTrasformo il neutral training set in immagini...')
         for i in tqdm(range(1, n_train+1)):
             # NEUTRAL - TRAIN
-            path_tmp_ntrain = path + 'NEUTRAL/TRAIN/' + str(i) + '.neutral.sim'
+            path_tmp_ntrain = path + 'DATASET/NEUTRAL/TRAIN/' + str(i) + '.neutral.sim'
             file_ntrain = Path(path_tmp_ntrain)
             if file_ntrain.is_file():
                 converter_fn(
-                            path = path + 'NEUTRAL/TRAIN/' + str(i) + '.neutral.sim',
-                            file_name = path + 'NEUTRAL/TRAIN_IMG/' + str(i) + '.neutral.png',
+                            path = path + 'DATASET/NEUTRAL/TRAIN/' + str(i) + '.neutral.sim',
+                            file_name = path + 'DATASET/NEUTRAL/TRAIN_IMG/' + str(i) + '.neutral.png',
                             )
 
         print('\nTrasformo il neutral test set in immagini...')
         for i in tqdm(range(1, n_test+1)):
             # NEUTRAL - TEST
-            path_tmp_ntest = path + 'NEUTRAL/TEST/' + str(i) + '.neutral.sim'
+            path_tmp_ntest = path + 'DATASET/NEUTRAL/TEST/' + str(i) + '.neutral.sim'
             file_ntest = Path(path_tmp_ntest)
             if file_ntest.is_file():
                 converter_fn(
-                            path = path + 'NEUTRAL/TEST/' + str(i) + '.neutral.sim',
-                            file_name = path + 'NEUTRAL/TEST_IMG/' + str(i) + '.neutral.png',
+                            path = path + 'DATASET/NEUTRAL/TEST/' + str(i) + '.neutral.sim',
+                            file_name = path + 'DATASET/NEUTRAL/TEST_IMG/' + str(i) + '.neutral.png',
                             )
 
-clean_tree(path = path, mode = mod)
+    if input('\nVuoi tenere le matrici in formato testuale? [Y]/[N]: ') == 'N':
+        os.system('rm -r ' + path + 'DATASET/SELECTION/TEST')
+        os.system('rm -r ' + path + 'DATASET/SELECTION/TRAIN')
 
-# if input('\nVuoi comprimere il dataset appena creato? [Y]/[N]: ') == 'Y':
-#     nome_archivio = input('Inserisci un nome da dare al file compresso: ')
-#     os.system('tar zcvf ' + nome_archivio + '.tar.gz ' + path)
+        os.system('rm -r ' + path + 'DATASET/NEUTRAL/TEST')
+        os.system('rm -r ' + path + 'DATASET/NEUTRAL/TRAIN')
+else:
+    clean_tree(path=path, mode=mod)
+
+if input('\nVuoi comprimere il dataset appena creato? [Y]/[N]: ') == 'Y':
+    print(path)
+    # nome_archivio = input('Inserisci un nome da dare al file compresso: ')
+    # os.system('tar zcvf ' + nome_archivio + '.tar.gz ' + path)
--- dataset_creator/foldertreeemng.py
+++ dataset_creator/foldertreeemng.py
@@ -1,100 +1,101 @@
 import os
 
+def create_tree(path, mode):
+    if os.access(path + 'DATASET', os.F_OK) == False:
+                os.mkdir(path + 'DATASET')
+
+    if mode == 'S' or mode == 'B':
+
+        if os.access(path + 'DATASET/SELECTION', os.F_OK) == True:
+            os.system('rm -r ' + path + 'DATASET/SELECTION')
+            os.mkdir(path + 'DATASET/SELECTION')
+        else:
+            os.mkdir(path + 'DATASET/SELECTION')
+
+        os.mkdir(path + 'DATASET/SELECTION/TRAIN/')
+        os.mkdir(path + 'DATASET/SELECTION/TEST/')
+        os.system('cp ' + path + 'ms2raster.zip ' + path + 'DATASET/SELECTION/TRAIN')
+        os.system('cd ' + path + 'DATASET/SELECTION/TRAIN' + ' ; unzip ms2raster.zip')
+        os.system('cp ' + path + 'ms2raster.zip ' + path + 'DATASET/SELECTION/TEST')
+        os.system('cd ' + path + 'DATASET/SELECTION/TEST' + ' ; unzip ms2raster.zip')
+
+    if mode == 'N' or mode == 'B':
+
+        if os.access(path + 'DATASET/NEUTRAL', os.F_OK) == True:
+            os.system('rm -r ' + path + 'DATASET/NEUTRAL')
+            os.mkdir(path + 'DATASET/NEUTRAL')
+        else:
+            os.mkdir(path + 'DATASET/NEUTRAL')
+
+        os.mkdir(path + 'DATASET/NEUTRAL/TRAIN/')
+        os.mkdir(path + 'DATASET/NEUTRAL/TEST/')
+        os.system('cp ' + path + 'ms2raster.zip ' + path + 'DATASET/NEUTRAL/TRAIN')
+        os.system('cd ' + path + 'DATASET/NEUTRAL/TRAIN' + ' ; unzip ms2raster.zip')
+        os.system('cp ' + path + 'ms2raster.zip ' + path + 'DATASET/NEUTRAL/TEST')
+        os.system('cd ' + path + 'DATASET/NEUTRAL/TEST' + ' ; unzip ms2raster.zip')
+
+def clean_tree(path, mode):
+    if mode == 'S' or mode =='B':
+
+        path_tmp = path + 'DATASET/SELECTION/'
+
+        os.system('cd ' + path_tmp + 'TRAIN ; rm ms2raster.zip')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm ms')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm lastp0')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm ms2raster.py')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm mssel')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm seedms')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm stepftn')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm tp.out')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm trajfixconst')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm -r __MACOSX')
+
+        os.system('cd ' + path_tmp + 'TEST ; rm ms2raster.zip')
+        os.system('cd ' + path_tmp + 'TEST ; rm ms')
+        os.system('cd ' + path_tmp + 'TEST ; rm lastp0')
+        os.system('cd ' + path_tmp + 'TEST ; rm ms2raster.py')
+        os.system('cd ' + path_tmp + 'TEST ; rm mssel')
+        os.system('cd ' + path_tmp + 'TEST ; rm seedms')
+        os.system('cd ' + path_tmp + 'TEST ; rm stepftn')
+        os.system('cd ' + path_tmp + 'TEST ; rm tp.out')
+        os.system('cd ' + path_tmp + 'TEST ; rm trajfixconst')
+        os.system('cd ' + path_tmp + 'TEST ; rm -r __MACOSX')
+    
+    if mode == 'N' or mode == 'B':
+
+        path_tmp = path + 'DATASET/NEUTRAL/'
+
+        os.system('cd ' + path_tmp + 'TRAIN ; rm ms2raster.zip')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm ms')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm lastp0')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm ms2raster.py')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm mssel')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm seedms')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm stepftn')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm tp.out')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm trajfixconst')
+        os.system('cd ' + path_tmp + 'TRAIN ; rm -r __MACOSX')
+
+        os.system('cd ' + path_tmp + 'TEST ; rm ms2raster.zip')
+        os.system('cd ' + path_tmp + 'TEST ; rm ms')
+        os.system('cd ' + path_tmp + 'TEST ; rm lastp0')
+        os.system('cd ' + path_tmp + 'TEST ; rm ms2raster.py')
+        os.system('cd ' + path_tmp + 'TEST ; rm mssel')
+        os.system('cd ' + path_tmp + 'TEST ; rm seedms')
+        os.system('cd ' + path_tmp + 'TEST ; rm stepftn')
+        os.system('cd ' + path_tmp + 'TEST ; rm tp.out')
+        os.system('cd ' + path_tmp + 'TEST ; rm trajfixconst')
+        os.system('cd ' + path_tmp + 'TEST ; rm -r __MACOSX')
+    
+
 # os.system('clear')
 # path = os.getcwd() + '/'
 # print('Path corrente: ' + path)
 
-# mod = input('\nAvviare in modalità SELECTION[S] o NEUTRAL[N]? ')
-
-def create_tree(path, mode):
-    if mode == 'S' or mode == 'B':
-            if os.access(path + 'SELECTION', os.F_OK) == True:
-                os.system('rm -r ' + path + 'SELECTION')
-                os.mkdir(path + 'SELECTION')
-                os.mkdir(path + 'SELECTION/TRAIN/')
-                os.mkdir(path + 'SELECTION/TEST/')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'SELECTION/TRAIN')
-                os.system('cd ' + path + 'SELECTION/TRAIN' + ' ; unzip ms2raster.zip')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'SELECTION/TEST')
-                os.system('cd ' + path + 'SELECTION/TEST' + ' ; unzip ms2raster.zip')
-            else:
-                os.mkdir(path + 'SELECTION')
-                os.mkdir(path + 'SELECTION/TRAIN/')
-                os.mkdir(path + 'SELECTION/TEST/')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'SELECTION/TRAIN')
-                os.system('cd ' + path + 'SELECTION/TRAIN' + ' ; unzip ms2raster.zip')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'SELECTION/TEST')
-                os.system('cd ' + path + 'SELECTION/TEST' + ' ; unzip ms2raster.zip')
-
-    if mode == 'N' or mode == 'B':
-            if os.access(path + 'NEUTRAL', os.F_OK) == True:
-                os.system('rm -r ' + path + 'NEUTRAL')
-                os.mkdir(path + 'NEUTRAL')
-                os.mkdir(path + 'NEUTRAL/TRAIN/')
-                os.mkdir(path + 'NEUTRAL/TEST/')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'NEUTRAL/TRAIN')
-                os.system('cd ' + path + 'NEUTRAL/TRAIN' + ' ; unzip ms2raster.zip')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'NEUTRAL/TEST')
-                os.system('cd ' + path + 'NEUTRAL/TEST' + ' ; unzip ms2raster.zip')
-            else:
-                os.mkdir(path + 'NEUTRAL')
-                os.mkdir(path + 'NEUTRAL/TRAIN/')
-                os.mkdir(path + 'NEUTRAL/TEST/')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'NEUTRAL/TRAIN')
-                os.system('cd ' + path + 'NEUTRAL/TRAIN' + ' ; unzip ms2raster.zip')
-                os.system('cp ' + path + 'ms2raster.zip ' + path + 'NEUTRAL/TEST')
-                os.system('cd ' + path + 'NEUTRAL/TEST' + ' ; unzip ms2raster.zip')
-
-def clean_tree(path, mode):
-    if mode == 'S' or mode =='B':
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm ms2raster.zip')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm ms')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm lastp0')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm ms2raster.py')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm mssel')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm seedms')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm stepftn')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm tp.out')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm trajfixconst')
-        os.system('cd ' + path + 'SELECTION/TRAIN ; rm -r __MACOSX')
-
-        os.system('cd ' + path + 'SELECTION/TEST ; rm ms2raster.zip')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm ms')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm lastp0')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm ms2raster.py')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm mssel')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm seedms')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm stepftn')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm tp.out')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm trajfixconst')
-        os.system('cd ' + path + 'SELECTION/TEST ; rm -r __MACOSX')
-    
-    if mode == 'N' or mode == 'B':
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm ms2raster.zip')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm ms')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm lastp0')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm ms2raster.py')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm mssel')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm seedms')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm stepftn')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm tp.out')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm trajfixconst')
-        os.system('cd ' + path + 'NEUTRAL/TRAIN ; rm -r __MACOSX')
-
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm ms2raster.zip')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm ms')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm lastp0')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm ms2raster.py')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm mssel')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm seedms')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm stepftn')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm tp.out')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm trajfixconst')
-        os.system('cd ' + path + 'NEUTRAL/TEST ; rm -r __MACOSX')
-    
-
-    
-
+# mod = input('\nAvviare in modalità SELECTION[S], NEUTRAL[N] o entrambe[B]?: ')
 # create_tree(path, mod)
-# clean_tree(path + 'SELECTION/')
+
+
+# clean_tree(path, mod)
+
 
