3143336a457f33820733b7b3850842ebf3570bf0
MatteoSid <matteo0593@gmail.com>
2019-02-04 14:31:54 +0100

    Finito log di creazione del dataset

--- dataset_creator/dataset_creator.py
+++ dataset_creator/dataset_creator.py
@@ -56,30 +56,30 @@ elif mod == 'N':
 else:
     log.write('- Il dataset è stato eseguito creando le matrici sia in modalità SELECTION che NEUTRAL.\n')
 log.write('- ' + str(int(n_train*2)) + ' matrici per il training (' + str(int(n_train)) + ' per tipo)\n')
-log.write('- ' + str(int(n_test*2)) + ' matrici per il test(' + str(int(n_test)) + ' per tipo)\n')
+log.write('- ' + str(int(n_test*2)) + ' matrici per il test (' + str(int(n_test)) + ' per tipo)\n')
 log.write('- le singole matrici hanno un bp di ' + n_mat + '\n')
 log.write("- l'intensità della selezione è " + selstr + '\n')
 log.write('- il numero di individui presi in considerazione è 24\n')
     
 if mod == 'S' or mod == 'B':
     comand_train = 'cd ' + path + 'DATASET/SELECTION/TRAIN ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(int(n_train)) + ' -l selection -selstr ' + selstr + ' -p ' + path + 'DATASET/SELECTION/TRAIN/ -i +24'
-    # log.write('Comando usato per generare il selection train set:\n' + comand_train + '\n\n')
+    log.write('\n\nComando usato per generare il selection train set:\n' + comand_train + '\n\n')
     # print('[LOG:comand_train]: ' + comand_train)
     # os.system(comand_train)   
     
     comand_test = 'cd ' + path + 'DATASET/SELECTION/TEST ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(int(n_test)) + ' -l selection -selstr ' + selstr + ' -p ' + path + 'DATASET/SELECTION/TEST/ -i +24'
-    # log.write('Comando usato per generare il selection test set:\npython3 ms2raster.py - bp ' + str(n_mat) + ' - s ' + str(n_test) + ' - l selection - selstr ' + selstr + ' - p ' + path + 'DATASET/SELECTION/TEST / -i + 24\n\n')
+    log.write('Comando usato per generare il selection test set:\n' + comand_test + '\n\n')
     # print('\nLOG:comand_test]: ' + comand_test)
     # os.system(comand_test)   
 
 if mod == 'N' or mod == 'B':
     comand_train = 'cd ' + path + 'DATASET/NEUTRAL/TRAIN ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(int(n_train)) + ' -l neutral -selstr ' + selstr + ' -p ' + path + 'DATASET/NEUTRAL/TRAIN/ -i +24'
-    # log.write('Comando usato per generare il neutral train set:\n' + comand_train + '\n\n')
+    log.write('Comando usato per generare il neutral train set:\n' + comand_train + '\n\n')
     # print('[LOG:comand_train]: ' + comand_train)
     # os.system(comand_train)   
     
     comand_test = 'cd ' + path + 'DATASET/NEUTRAL/TEST ; python3 ms2raster.py -bp ' + str(n_mat) + ' -s ' + str(int(n_test)) + ' -l neutral -selstr ' + selstr + ' -p ' + path + 'DATASET/NEUTRAL/TEST/ -i +24'
-    # log.write('Comando usato per generare il neutral test set:\n' + comand_test + '\n\n')
+    log.write('Comando usato per generare il neutral test set:\n' + comand_test + '\n\n')
     # print('\nLOG:comand_test]: ' + comand_test)
     # os.system(comand_test)
 
@@ -90,7 +90,7 @@ if input('\nVuoi trasformare le matrici in immagini? [Y]/[N]: ') == 'Y':
         os.mkdir(path + 'DATASET/SELECTION/TEST_IMG')
 
         print('\nTrasformo il selection training set in immagini...')
-        for i in tqdm(range(1, n_train+1)):
+        for i in tqdm(range(1, int(n_train)+1)):
             # SELECTION - TRAIN
             path_tmp_strain = path + 'DATASET/SELECTION/TRAIN/' + str(i) + '.selection.sim'
             file_strain = Path(path_tmp_strain)
@@ -101,7 +101,7 @@ if input('\nVuoi trasformare le matrici in immagini? [Y]/[N]: ') == 'Y':
                             )
 
         print('\nTrasformo il selection test set in immagini...')
-        for i in tqdm(range(1, n_test+1)):
+        for i in tqdm(range(1, int(n_test)+1)):
             # SELECTION - TEST
             path_tmp_stest = path + 'DATASET/SELECTION/TEST/' + str(i) + '.selection.sim'
             file_stest = Path(path_tmp_stest)
@@ -115,7 +115,7 @@ if input('\nVuoi trasformare le matrici in immagini? [Y]/[N]: ') == 'Y':
         os.mkdir(path + 'DATASET/NEUTRAL/TEST_IMG')
 
         print('\nTrasformo il neutral training set in immagini...')
-        for i in tqdm(range(1, n_train+1)):
+        for i in tqdm(range(1, int(n_train)+1)):
             # NEUTRAL - TRAIN
             path_tmp_ntrain = path + 'DATASET/NEUTRAL/TRAIN/' + str(i) + '.neutral.sim'
             file_ntrain = Path(path_tmp_ntrain)
@@ -126,7 +126,7 @@ if input('\nVuoi trasformare le matrici in immagini? [Y]/[N]: ') == 'Y':
                             )
 
         print('\nTrasformo il neutral test set in immagini...')
-        for i in tqdm(range(1, n_test+1)):
+        for i in tqdm(range(1, int(n_test)+1)):
             # NEUTRAL - TEST
             path_tmp_ntest = path + 'DATASET/NEUTRAL/TEST/' + str(i) + '.neutral.sim'
             file_ntest = Path(path_tmp_ntest)
@@ -147,5 +147,5 @@ else:
 
 if input('\nVuoi comprimere il dataset appena creato? [Y]/[N]: ') == 'Y':
     print(path)
-    # nome_archivio = input('Inserisci un nome da dare al file compresso: ')
-    # os.system('tar zcvf ' + nome_archivio + '.tar.gz ' + path)
+    nome_archivio = input('Inserisci un nome da dare al file compresso: ')
+    os.system('tar zcvf ' + nome_archivio + '.tar.gz ' + path)
